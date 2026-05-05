2258e42d8fb9e4785e0b4c4e43bb67230baae63a
samjose007 <samueljosetjia777@gmail.com>
2026-05-05 21:12:53 +0700

    Add GitHub Pages to CORS allowed origins

--- backend-supply-chain/main.py
+++ backend-supply-chain/main.py
@@ -13,7 +13,7 @@ load_dotenv()
 app = FastAPI(title="Secure Supply Chain API")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:5500", "http://localhost:5500"], 
+    allow_origins=["http://127.0.0.1:5500", "http://localhost:5500", "https://samjose007.github.io"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
